568f80ac7a2ed8704a91fb789439ba8c8b12c3cc
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-11 21:40:39 +0530

    Apps Routing Created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Repos from './pages/Repos'
+import Home from './pages/Home'
 
 function App() {
-  
+
 
   return (
     <>
-      
-     <h1>GitHubGround</h1>
-     <hr />
+
+      <h1>GitHubGround</h1>
+      <hr />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/repos' element={<Repos />} />
+      </Routes>
     </>
   )
 }
