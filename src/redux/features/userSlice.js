1583101fd5aab011b91ab75f7272d1e54bf9b80c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-11 21:34:58 +0530

    Home Page Created.

---
+++ src/redux/features/userSlice.js
@@ -0,0 +1,15 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const userSlice = createSlice({
+//     name:"user", 
+//     initialState: {
+//         username: ""
+//     },
+
+//     reducers: {
+
+//         setUserName :(state, payload)=>{
+//             state.username = payload.username;
+//         }
+//     }
+// })
