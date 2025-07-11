f72a11bb93a8a7fa06cef36746b04f61eb7f4ccc
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-11 23:13:42 +0530

    Header And Footer Styled.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
             console.log(data);
             
         } catch (error) {
-            console.log(error);
+            console.log(error.message);
         }
     }
     return (
