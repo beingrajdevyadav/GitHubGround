6804611076eb1360f644ceeb7913882de841b8ac
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-12 19:28:07 +0530

    View Profile Button Functionality Added.

--- src/pages/Ground.jsx
+++ src/pages/Ground.jsx
@@ -2,25 +2,31 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Ground = () => {
-    const user = useSelector((state)=>state.user);
+    const user = useSelector((state) => state.user);
     console.log(user);
-  return (
-    <div className='ground'>
-        <div className="infos">
-            <div className="profile-pic">
-            <img src={user.avatar_url} alt="" />
+
+    const viewProfile = () => {
+        window.open(user.html_url, "_blank");
+    };
+
+
+    return (
+        <div className='ground'>
+            <div className="infos">
+                <div className="profile-pic">
+                    <img src={user.avatar_url} alt="" />
+                </div>
+
+                <h2>{user.name}</h2>
+                <p>{user.login}</p>
+
+                <p>{user.bio}</p>
+
+                <button onClick={viewProfile}>View Profile</button>
             </div>
-
-            <h2>{user.name}</h2>
-            <p>{user.login}</p>
-
-            <p>{user.bio}</p>
-
-            <button>View Profile</button>
+            <div className="reports"></div>
         </div>
-        <div className="reports"></div>
-    </div>
-  )
+    )
 }
 
 export default Ground
