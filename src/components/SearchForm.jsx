62acc2a9f0fdd0552363dcc35e41dfdfb0b57af1
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-12 10:37:23 +0530

    Search Functionality Added In SearchForm.

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,18 +1,32 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 const SearchForm = () => {
   const [username, setUsername] = useState("");
+
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+
+    try {
+      const {data} = await axios.get(`https://api.github.com/users/${username}`);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className='search-form'>
 
       <div className="search-img">
         <img src="https://media.tenor.com/A15H8E1VUh8AAAAM/github-cat.gif" alt="" />
       </div>
-      <form action="">
+
+      <form action="" onSubmit={handleSubmit}>
         <h3>Welcome <br /> To GitHubGround</h3>
 
         <div className="form-control">
-          <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)}  placeholder='Search A GitHub Account' />
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Search A GitHub Account' />
         </div>
 
         <div className="btn-control">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,14 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { toast } from 'react-toastify'
+
+import React from 'react'
+
 import SearchForm from '../components/SearchForm';
 
 const Home = () => {
-    const [username, setUsername] = useState("");
-
-    const fetchGitHubUser = async (username) => {
-
-
-        try {
-
-            const { data } = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(data);
-
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
+    
     return (
-        <div>
-            {/* <input type="text" placeholder='Enter User Name' value={username} onChange={(e) => { setUsername(e.target.value) }} />
-
-            <button onClick={() => fetchGitHubUser(username)}>Search</button> */}
-
+        <>
             <SearchForm/>
-        </div>
+        </>
     )
 }
 
