a81fc0413b1d493afea6c06042b0682c9bada57d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-13 03:51:58 +0530

    Public Repos Rendered Successfully.

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const Repos = () => {
-  const user = useSelector(state=>state.user);
+  const user = useSelector(state => state.user);
   console.log(user);
+
+  const [repos, setRepos] = useState([]);
+
+
+  const fetchRepos = async () => {
+    try {
+      const { data } = await axios.get(`${user.repos_url}`);
+      setRepos(data);
+      // console.log(data);
+    } catch (error) {
+      console.log(error.response?.status);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
   return (
-    <div>Repos</div>
+    <div className='repos'>
+      <div className="user-info">
+        <img src={user.avatar_url} alt="" />
+
+        <div className="user-details">
+          <h2>{user.name}</h2>
+          <p>{user.login}</p>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+
+      <div className="repos-list">
+        {
+          repos?.map((rep, i) => (
+            <div className='repo' key={i}>
+              <h4>{rep.name}</h4>
+              <p>{rep.description}</p>
+              <div className="repo-actions">
+                <button>Clone </button>
+                <button>Open </button>
+                <button>Download</button>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
